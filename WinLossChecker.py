84661e74030878f7461952188ff6f2608705fd40
stoneG <me@sitong>
2012-10-01 15:51:30 -0400

    adding the first iteration of the  main program

---
+++ WinLossChecker.py
@@ -0,0 +1,218 @@
+# Checks that the Grand Slam singles performance of professional tennis players on
+# Wikipedia matches their win/loss ratios.
+
+import re, numpy
+
+OUTCOME = ['1R', '2R', '3R', '4R', 'QF', 'SF', 'F', 'W']
+wins = dict(zip(OUTCOME, range(0,8))) # Gives # wins for round attained
+
+# The following functions work together to construct the win-loss record
+
+def totalWins(li):
+    """ Given a list of rounds, return the total number of wins"""
+    W = 0
+    for stage in li:
+        if stage not in wins.keys():
+            continue
+        W += wins[stage]
+    return(W)
+
+def totalLosses(li):
+    """ Given a list of rounds, return the total number of losses"""
+    L = 0
+    for stage in li:
+        if stage == 'W':
+            continue
+        else:
+            L += 1
+    return(L)
+
+def winLoss(li):
+    """ Given a list of rounds, return win and loss Record"""
+    WL = str(totalWins(li)) + '-' + str(totalLosses(li))
+    return(WL)
+
+# Build list of slam performance
+
+performance = re.findall(r"– Men\'s Singles\|\'*([^\]]+)",djok)
+i = 0
+while i < len(performance):
+    if len(performance[i]) > 2:
+        performance.remove(performance[i])
+    else:
+        i += 1
+del i
+
+# Add in emtpy strings for yet to be played slams in current year
+
+UNPLAYED = 4 - len(performance) % 4
+i = 0
+while i < UNPLAYED:
+    performance.append('')
+    i += 1
+del i
+
+# Build array of performances
+
+perfSlamArray = array(performance).reshape(4,len(performance)/4)
+perfYearArray = perfSlamArray.transpose()
+
+# Extract win/loss performance for each slam (AO, RG, W, USO)
+
+slamWL = []
+for arr in perfSlamArray:
+    slamWL.append(winLoss(list(arr)))
+
+yearWL = []
+for arr in perfYearArray:
+    yearWL.append(winLoss(list(arr)))
+
+
+"""
+array methods that i'll need:
+.sum(axis=0) gives you list of column sums
+.min(axis=1) gives you mins of rows
+.cumsum(axis=1) gives you cumulative sum along each row
+
+.transpose() gives you transpose
+[1] gives you 2nd row as a list, but you can coerce into a list using list()
+
+.ravel unravels the array/flattens the array
+"""
+
+
+"""
+IDEAS:
+
+1. Build a dict/structure
+    KEYS: AO, FO, W, USO
+    VALUES: [(2005, 2R), (2006, 3R), (2007, F)]
+
+   Extract performance
+    AO Slam perf: List of 2nd tuple value in each dict entry for AO
+    2007 Slam perf: List of second tuple value of first tuple for each key
+
+2. Data Matrix (or list within list)
+     2005 2006 2007 2008
+AO    1R   3R   F    W
+FO
+W               etc..
+USO
+
+   Then, extract.
+   
+"""
+
+djok = u"""===Grand Slam tournament performance timeline===
+''To prevent confusion and double counting, information in this table is updated only once a tournament or the player's participation in the tournament has concluded. This table is current through the [[2012 Wimbledon Championships]].''
+<!--Regarding the italicized note, it just means wait until he loses in the tournament, or until he wins it before updating numbers, so an editor can do it in just one go. and in updating numbers, include the update of tournaments played, finals reached and won, surface win–loss, overall win–loss, and these numbers in the career column, as well as the win–loss in the footnote. -->
+
+{|class=wikitable style=text-align:center;font-size:98%
+|-
+!Tournament!![[2003 ATP Tour|2003]]!![[2004 ATP Tour|2004]]!![[2005 ATP Tour|2005]]!![[2006 ATP Tour|2006]]!![[2007 ATP Tour|2007]]!![[2008 ATP Tour|2008]]!![[2009 ATP World Tour|2009]]!![[2010 ATP World Tour|2010]]!![[2011 ATP World Tour|2011]]!![[2012 ATP World Tour|2012]]!!width=50|SR!!width=50|W–L!!width=50|Win %
+|-
+| colspan="14" style="text-align:left;"|'''Grand Slam Tournaments
+|-
+| style="background:#efefef;" text-align:left|Australian Open
+|A
+|A
+| style="background:#afeeee;"|[[2005 Australian Open – Men's Singles|1R]]
+| style="background:#afeeee;"|[[2006 Australian Open – Men's Singles|1R]]
+| style="background:#afeeee;"|[[2007 Australian Open – Men's Singles|4R]]
+|bgcolor=lime|[[2008 Australian Open – Men's Singles|'''W]]
+| style="background:#ffebcd;"|[[2009 Australian Open – Men's Singles|QF]]
+| style="background:#ffebcd;"|[[2010 Australian Open – Men's Singles|QF]]
+|bgcolor=lime|[[2011 Australian Open – Men's Singles|'''W]]
+|bgcolor=lime|[[2012 Australian Open – Men's Singles|'''W]]
+| style="background:#efefef;"|3 / 8
+| style="background:#efefef;"|32–5
+| style="background:#efefef;"|86.49
+|-
+| style="background:#efefef; text-align:left;"|French Open
+|A
+|A
+| style="background:#afeeee;"|[[2005 French Open – Men's Singles|2R]]
+| style="background:#ffebcd;"|[[2006 French Open – Men's Singles|QF]]
+| style="background:yellow;"|[[2007 French Open – Men's Singles|SF]]
+| style="background:yellow;"|[[2008 French Open – Men's Singles|SF]]
+| style="background:#afeeee;"|[[2009 French Open – Men's Singles|3R]]
+| style="background:#ffebcd;"|[[2010 French Open – Men's Singles|QF]]
+| style="background:yellow;"|[[2011 French Open – Men's Singles|SF]]
+| style="background:thistle;"|[[2012 French Open – Men's Singles|F]]
+| style="background:#efefef;"|0 / 8
+| style="background:#efefef;"|31–8
+| style="background:#efefef;"|79.49
+|-
+| style="background:#efefef; text-align:left;"|Wimbledon
+|A
+|A
+| style="background:#afeeee;"|[[2005 Wimbledon Championships – Men's Singles|3R]]
+| style="background:#afeeee;"|[[2006 Wimbledon Championships – Men's Singles|4R]]
+| style="background:yellow;"|[[2007 Wimbledon Championships – Men's Singles|SF]]
+| style="background:#afeeee;"|[[2008 Wimbledon Championships – Men's Singles|2R]]
+| style="background:#ffebcd;"|[[2009 Wimbledon Championships – Men's Singles|QF]]
+| style="background:yellow;"|[[2010 Wimbledon Championships – Men's Singles|SF]]
+|bgcolor=lime|[[2011 Wimbledon Championships – Men's Singles|'''W]]
+| style="background:yellow;"|[[2012 Wimbledon Championships – Men's Singles|SF]]
+| style="background:#efefef;"|1 / 8
+| style="background:#efefef;"|32–7
+| style="background:#efefef;"|82.05
+|-
+| style="background:#efefef; text-align:left;"|US Open
+|A
+|A
+| style="background:#afeeee;"|[[2005 US Open – Men's Singles|3R]]
+| style="background:#afeeee;"|[[2006 US Open – Men's Singles|3R]]
+| style="background:thistle;"|[[2007 US Open – Men's Singles|F]]
+| style="background:yellow;"|[[2008 US Open – Men's Singles|SF]]
+| style="background:yellow;"|[[2009 US Open – Men's Singles|SF]]
+| style="background:thistle;"|[[2010 US Open – Men's Singles|F]]
+|bgcolor=lime|[[2011 US Open – Men's Singles|'''W]]
+|bgcolor=|
+| style="background:#efefef;"|1 / 7
+| style="background:#efefef;"|33–6
+| style="background:#efefef;"|84.62
+|-
+!style=text-align:left|Win–Loss
+!0–0
+!0–0
+!5–4
+!9–4
+!19–4
+!18–3
+!15–4
+!19–4
+!25–1
+!18–2
+!5 / 31
+!128–26
+!83.12
+|}
+
+;<big>Finals: 8 (5 titles, 3 runners-up)</big>
+{|class="sortable wikitable"
+|-
+!width=80|Outcome
+!width=50|Year
+!width=200|Championship
+!width=75|Surface
+!width=200|Opponent in the final
+!width=200|Score in the final
+|- style="background:#ccf;"
+|bgcolor=FFA07A|Runner-up||[[2007 US Open – Men's Singles|2007]]||[[US Open (tennis)|US Open]] <small>(1)||Hard||{{flagicon|SWI}} [[Roger Federer]]||6–7<sup>(4–7)</sup>, 6–7<sup>(2–7)</sup>, 4–6
+|- style="background:#ffc;"
+|bgcolor=98FB98|Winner||[[2008 Australian Open – Men's Singles|2008]]||[[Australian Open]] <small>(1)||Hard||{{flagicon|FRA}} [[Jo-Wilfried Tsonga]]||4–6, 6–4, 6–3, 7–6<sup>(7–2)
+|- style="background:#ccf;"
+|bgcolor=FFA07A|Runner-up||[[2010 US Open – Men's Singles|2010]]||US Open <small>(2)||Hard||{{flagicon|ESP}} [[Rafael Nadal]]||4–6, 7–5, 4–6, 2–6
+|- style="background:#ffc;"
+|bgcolor=98FB98|Winner||[[2011 Australian Open – Men's Singles|2011]]||Australian Open <small>(2)||Hard||{{flagicon|GBR}} [[Andy Murray]]||6–4, 6–2, 6–3
+|- style="background:#cfc;"
+| style="background:#98fb98;"|Winner||[[2011 Wimbledon Championships|2011]] || [[Wimbledon Championships|Wimbledon]] <small>(1)|| Grass ||{{flagicon|ESP}} Rafael Nadal || 6–4, 6–1, 1–6, 6–3
+|- style="background:#ccf;"
+|bgcolor=98FB98|Winner||[[2011 US Open – Men's Singles|2011]]||US Open <small>(1)||Hard||{{flagicon|ESP}} Rafael Nadal||6–2, 6–4, 6–7<sup>(3–7)</sup>, 6–1
+|- style="background:#ffc;"
+|bgcolor=98FB98|Winner||[[2012 Australian Open – Men's Singles|2012]]||Australian Open <small>(3)||Hard||{{flagicon|ESP}} Rafael Nadal||5–7, 6–4, 6–2, 6–7<sup>(5–7)</sup>, 7–5
+|- style="background:#ebc2af;"
+|bgcolor=FFA07A|Runner-up||[[2012 French Open – Men's Singles|2012]]||[[French Open]] <small>(1)||Clay||{{flagicon|ESP}} Rafael Nadal||4–6, 3–6, 6–2, 5–7
+|}
+"""
